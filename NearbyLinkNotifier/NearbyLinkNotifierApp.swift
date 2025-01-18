b4fef8768c7dc71db0520fe84fadf4f77ce9b977
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2025-01-18 20:21:40 +0100

    Initial Commit

---
+++ NearbyLinkNotifier/NearbyLinkNotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyLinkNotifierApp.swift
+//  NearbyLinkNotifier
+//
+//  Created by Maximilian Arnold on 18.01.25.
+//
+
+import SwiftUI
+
+@main
+struct NearbyLinkNotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NearbyLinkNotifierTests/NearbyLinkNotifierTests.swift
@@ -0,0 +1,16 @@
+//
+//  NearbyLinkNotifierTests.swift
+//  NearbyLinkNotifierTests
+//
+//  Created by Maximilian Arnold on 18.01.25.
+//
+
+import Testing
+
+struct NearbyLinkNotifierTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
